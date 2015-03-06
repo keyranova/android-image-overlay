c7ca1781492c54ca4577e025c130698f9a557f26
browntown412 <k.novack88@gmail.com>
2015-03-05 21:41:19 -0500

    removes unused imports

--- app/src/main/java/com/kurtnovack/imageoverlay/MainActivity.java
+++ app/src/main/java/com/kurtnovack/imageoverlay/MainActivity.java
@@ -7,23 +7,14 @@ import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
-import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-
 
 public class MainActivity extends ActionBarActivity {
     private static final int IMAGE_CAPTURE = 1;
